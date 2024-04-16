99296d12f00996dce280fd127a711285716cf21b
Lidiane Trindade <47565259+lstrindade@users.noreply.github.com>
2024-04-16 14:06:58 -0300

    Update create_card.py

--- create_card.py
+++ create_card.py
@@ -2,20 +2,14 @@ import cv2
 
 img = cv2.imread("poster.jpg")
 
-rocket = img[120:360,400:500]
+rocket = img[120:360, 400:500]
 
 img[0:240, 500:600] = rocket
 
 text_to_show = "Houston we have a problem..."
 
 
-cv2.putText(img,  
-           text_to_show,
-           (20, 220),  
-           fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
-           fontScale=0.6,  
-           color=(0,0,255)
-           )
+
 
 cv2.imshow("resultado",img)
 
